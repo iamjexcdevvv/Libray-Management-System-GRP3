1b084b950e9262a41519369eb810e89a66343dda
iamjexcdevvv <dublasandrei@gmail.com>
2025-03-03 20:02:28 +0800

    reimplements admin dashboard UI

--- forms/admin_dashboard.Designer.vb
+++ forms/admin_dashboard.Designer.vb
@@ -22,30 +22,255 @@ Partial Class admin_dashboard
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(admin_dashboard))
+        Panel1 = New Panel()
+        Label2 = New Label()
         Label1 = New Label()
+        PictureBox2 = New PictureBox()
+        PictureBox1 = New PictureBox()
+        Panel2 = New Panel()
+        Label3 = New Label()
+        Panel3 = New Panel()
+        TextBox1 = New TextBox()
+        Panel4 = New Panel()
+        Label4 = New Label()
+        Panel5 = New Panel()
+        TextBox2 = New TextBox()
+        Panel6 = New Panel()
+        Label5 = New Label()
+        Button1 = New Button()
+        Panel1.SuspendLayout()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        Panel2.SuspendLayout()
+        Panel3.SuspendLayout()
+        Panel4.SuspendLayout()
+        Panel5.SuspendLayout()
+        Panel6.SuspendLayout()
         SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.FromArgb(CByte(67), CByte(85), CByte(244))
+        Panel1.Controls.Add(Label2)
+        Panel1.Controls.Add(Label1)
+        Panel1.Controls.Add(PictureBox2)
+        Panel1.Controls.Add(PictureBox1)
+        Panel1.Controls.Add(Panel2)
+        Panel1.Location = New Point(0, 1)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(283, 449)
+        Panel1.TabIndex = 0
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label2.ForeColor = Color.WhiteSmoke
+        Label2.Location = New Point(73, 161)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(147, 18)
+        Label2.TabIndex = 5
+        Label2.Text = "Manage Students"
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Font = New Font("Showcard Gothic", 12.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        Label1.Location = New Point(336, 147)
+        Label1.Font = New Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = Color.WhiteSmoke
+        Label1.Location = New Point(73, 85)
         Label1.Name = "Label1"
-        Label1.Size = New Size(121, 20)
-        Label1.TabIndex = 0
-        Label1.Text = "Hello World"
+        Label1.Size = New Size(125, 18)
+        Label1.TabIndex = 4
+        Label1.Text = "Manage Books"
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Cursor = Cursors.Hand
+        PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), Image)
+        PictureBox2.Location = New Point(12, 150)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(55, 42)
+        PictureBox2.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox2.TabIndex = 3
+        PictureBox2.TabStop = False
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Cursor = Cursors.Hand
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(12, 71)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(55, 45)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 2
+        PictureBox1.TabStop = False
+        ' 
+        ' Panel2
+        ' 
+        Panel2.BackColor = Color.FromArgb(CByte(238), CByte(115), CByte(12))
+        Panel2.Controls.Add(Label3)
+        Panel2.Location = New Point(0, 0)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(283, 54)
+        Panel2.TabIndex = 1
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Arial Rounded MT Bold", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label3.ForeColor = Color.FromArgb(CByte(36), CByte(45), CByte(88))
+        Label3.Location = New Point(39, 19)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(197, 22)
+        Label3.TabIndex = 5
+        Label3.Text = "Admin Control Panel"
+        ' 
+        ' Panel3
+        ' 
+        Panel3.BackColor = SystemColors.ActiveCaption
+        Panel3.Controls.Add(TextBox1)
+        Panel3.Controls.Add(Panel4)
+        Panel3.Location = New Point(314, 20)
+        Panel3.Name = "Panel3"
+        Panel3.Size = New Size(200, 118)
+        Panel3.TabIndex = 1
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = SystemColors.ActiveCaption
+        TextBox1.BorderStyle = BorderStyle.None
+        TextBox1.Font = New Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox1.Location = New Point(3, 66)
+        TextBox1.Name = "TextBox1"
+        TextBox1.ReadOnly = True
+        TextBox1.Size = New Size(194, 19)
+        TextBox1.TabIndex = 3
+        TextBox1.TabStop = False
+        TextBox1.Text = "0"
+        TextBox1.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' Panel4
+        ' 
+        Panel4.BackColor = Color.FromArgb(CByte(67), CByte(85), CByte(244))
+        Panel4.Controls.Add(Label4)
+        Panel4.Location = New Point(0, 0)
+        Panel4.Name = "Panel4"
+        Panel4.Size = New Size(200, 41)
+        Panel4.TabIndex = 2
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Arial Rounded MT Bold", 9.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label4.ForeColor = Color.WhiteSmoke
+        Label4.Location = New Point(3, 20)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(116, 15)
+        Label4.TabIndex = 5
+        Label4.Text = "Number of Books"
+        ' 
+        ' Panel5
+        ' 
+        Panel5.BackColor = SystemColors.ActiveCaption
+        Panel5.Controls.Add(TextBox2)
+        Panel5.Controls.Add(Panel6)
+        Panel5.Location = New Point(537, 20)
+        Panel5.Name = "Panel5"
+        Panel5.Size = New Size(200, 118)
+        Panel5.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.BackColor = SystemColors.ActiveCaption
+        TextBox2.BorderStyle = BorderStyle.None
+        TextBox2.Font = New Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox2.Location = New Point(3, 66)
+        TextBox2.Name = "TextBox2"
+        TextBox2.ReadOnly = True
+        TextBox2.Size = New Size(194, 19)
+        TextBox2.TabIndex = 4
+        TextBox2.TabStop = False
+        TextBox2.Text = "0"
+        TextBox2.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' Panel6
+        ' 
+        Panel6.BackColor = Color.FromArgb(CByte(67), CByte(85), CByte(244))
+        Panel6.Controls.Add(Label5)
+        Panel6.Location = New Point(0, 0)
+        Panel6.Name = "Panel6"
+        Panel6.Size = New Size(200, 41)
+        Panel6.TabIndex = 2
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Arial Rounded MT Bold", 9.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label5.ForeColor = Color.WhiteSmoke
+        Label5.Location = New Point(3, 20)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(115, 15)
+        Label5.TabIndex = 5
+        Label5.Text = "Number of Users"
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.Red
+        Button1.Cursor = Cursors.Hand
+        Button1.FlatStyle = FlatStyle.Popup
+        Button1.Font = New Font("Arial Rounded MT Bold", 9.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button1.ForeColor = Color.WhiteSmoke
+        Button1.Location = New Point(610, 392)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 27)
+        Button1.TabIndex = 3
+        Button1.Text = "Logout"
+        Button1.UseVisualStyleBackColor = False
         ' 
         ' admin_dashboard
         ' 
-        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
-        Controls.Add(Label1)
+        Controls.Add(Button1)
+        Controls.Add(Panel5)
+        Controls.Add(Panel3)
+        Controls.Add(Panel1)
         Name = "admin_dashboard"
+        StartPosition = FormStartPosition.CenterScreen
         Text = "Admin Dashboard"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        Panel2.ResumeLayout(False)
+        Panel2.PerformLayout()
+        Panel3.ResumeLayout(False)
+        Panel3.PerformLayout()
+        Panel4.ResumeLayout(False)
+        Panel4.PerformLayout()
+        Panel5.ResumeLayout(False)
+        Panel5.PerformLayout()
+        Panel6.ResumeLayout(False)
+        Panel6.PerformLayout()
         ResumeLayout(False)
-        PerformLayout()
     End Sub
 
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label2 As Label
     Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents Panel6 As Panel
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
 End Class
