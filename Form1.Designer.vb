69211765c5be87dcaae2b50bda6a6badf4a53a38
iamjexcdevvv <dublasandrei@gmail.com>
2025-03-04 17:32:30 +0800

    removed unnecessary files

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -30,15 +30,15 @@ Partial Class Form1
         PictureBox1 = New PictureBox()
         TextBox3 = New TextBox()
         Splitter1 = New Splitter()
+        Button2 = New Button()
         CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' Button1
         ' 
-        Button1.Location = New Point(440, 421)
-        Button1.Margin = New Padding(4, 5, 4, 5)
+        Button1.Location = New Point(308, 253)
         Button1.Name = "Button1"
-        Button1.Size = New Size(191, 52)
+        Button1.Size = New Size(134, 31)
         Button1.TabIndex = 0
         Button1.Text = "Login"
         Button1.UseVisualStyleBackColor = False
@@ -49,9 +49,10 @@ Partial Class Form1
         Label1.Font = New Font("Arial Rounded MT Bold", 16.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Label1.ForeColor = SystemColors.ActiveCaptionText
         Label1.ImageAlign = ContentAlignment.MiddleRight
-        Label1.Location = New Point(326, 211)
+        Label1.Location = New Point(228, 127)
+        Label1.Margin = New Padding(2, 0, 2, 0)
         Label1.Name = "Label1"
-        Label1.Size = New Size(164, 32)
+        Label1.Size = New Size(129, 26)
         Label1.TabIndex = 1
         Label1.Text = "Username:"
         ' 
@@ -60,9 +61,10 @@ Partial Class Form1
         Label2.AutoSize = True
         Label2.Font = New Font("Arial Rounded MT Bold", 16.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Label2.ForeColor = SystemColors.ActiveCaptionText
-        Label2.Location = New Point(326, 310)
+        Label2.Location = New Point(228, 186)
+        Label2.Margin = New Padding(2, 0, 2, 0)
         Label2.Name = "Label2"
-        Label2.Size = New Size(159, 32)
+        Label2.Size = New Size(126, 26)
         Label2.TabIndex = 2
         Label2.Text = "Password:"
         ' 
@@ -70,27 +72,31 @@ Partial Class Form1
         ' 
         TextBox1.BackColor = Color.OrangeRed
         TextBox1.Font = New Font("Arial Rounded MT Bold", 16.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        TextBox1.Location = New Point(326, 255)
+        TextBox1.Location = New Point(228, 153)
+        TextBox1.Margin = New Padding(2)
         TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(385, 39)
+        TextBox1.Size = New Size(271, 32)
         TextBox1.TabIndex = 3
         ' 
         ' TextBox2
         ' 
         TextBox2.BackColor = Color.OrangeRed
         TextBox2.Font = New Font("Arial Rounded MT Bold", 16.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        TextBox2.Location = New Point(326, 363)
+        TextBox2.Location = New Point(228, 218)
+        TextBox2.Margin = New Padding(2)
         TextBox2.Name = "TextBox2"
-        TextBox2.Size = New Size(390, 39)
+        TextBox2.PasswordChar = "*"c
+        TextBox2.Size = New Size(274, 32)
         TextBox2.TabIndex = 4
         ' 
         ' PictureBox1
         ' 
         PictureBox1.BackgroundImageLayout = ImageLayout.Center
         PictureBox1.Image = My.Resources.Resources._42b57666dbe879a032955b85c5dcdcd5
-        PictureBox1.Location = New Point(185, 80)
+        PictureBox1.Location = New Point(130, 48)
+        PictureBox1.Margin = New Padding(2)
         PictureBox1.Name = "PictureBox1"
-        PictureBox1.Size = New Size(123, 123)
+        PictureBox1.Size = New Size(86, 74)
         PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
         PictureBox1.TabIndex = 5
         PictureBox1.TabStop = False
@@ -100,9 +106,10 @@ Partial Class Form1
         TextBox3.BackColor = Color.OrangeRed
         TextBox3.Font = New Font("Arial Rounded MT Bold", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         TextBox3.ForeColor = SystemColors.InactiveCaptionText
-        TextBox3.Location = New Point(12, 12)
+        TextBox3.Location = New Point(8, 7)
+        TextBox3.Margin = New Padding(2)
         TextBox3.Name = "TextBox3"
-        TextBox3.Size = New Size(1173, 62)
+        TextBox3.Size = New Size(822, 51)
         TextBox3.TabIndex = 6
         TextBox3.Text = "Log In Form, Welcome !"
         TextBox3.TextAlign = HorizontalAlignment.Center
@@ -110,19 +117,30 @@ Partial Class Form1
         ' Splitter1
         ' 
         Splitter1.Location = New Point(0, 0)
+        Splitter1.Margin = New Padding(2)
         Splitter1.Name = "Splitter1"
-        Splitter1.Size = New Size(4, 750)
+        Splitter1.Size = New Size(3, 421)
         Splitter1.TabIndex = 7
         Splitter1.TabStop = False
         ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(507, 226)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 8
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = SystemColors.MenuHighlight
         BackgroundImage = My.Resources.Resources.d49ed28d2d1e9861b772d1318e38956d
         BackgroundImageLayout = ImageLayout.Center
-        ClientSize = New Size(1197, 750)
+        ClientSize = New Size(838, 421)
+        Controls.Add(Button2)
         Controls.Add(Splitter1)
         Controls.Add(TextBox3)
         Controls.Add(PictureBox1)
@@ -131,7 +149,6 @@ Partial Class Form1
         Controls.Add(Label2)
         Controls.Add(Label1)
         Controls.Add(Button1)
-        Margin = New Padding(4, 5, 4, 5)
         Name = "Form1"
         Text = "Form1"
         CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
@@ -147,5 +164,6 @@ Partial Class Form1
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents TextBox3 As TextBox
     Friend WithEvents Splitter1 As Splitter
+    Friend WithEvents Button2 As Button
 
 End Class
