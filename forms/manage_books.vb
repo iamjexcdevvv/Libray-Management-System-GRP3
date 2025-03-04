f0ffa7edebe0016becb4f4e62c44d442e791765d
iamjexcdevvv <dublasandrei@gmail.com>
2025-03-04 08:39:18 +0800

    implements manage book form

---
+++ forms/manage_books.vb
@@ -0,0 +1,9 @@
+﻿Public Class manage_books
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+End Class
