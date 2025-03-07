7642067b4682d4cad33bf6fc3153a9308efda2a2
zid-art <kyle.games0716@gmial.com>
2025-03-07 19:22:00 +0900

    Registration Design

--- Form2.Designer.vb
+++ Form2.Designer.vb
@@ -22,9 +22,229 @@ Partial Class Form2
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form2"
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form2))
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        GroupBox1 = New GroupBox()
+        CheckBox2 = New CheckBox()
+        CheckBox1 = New CheckBox()
+        Label11 = New Label()
+        TextBox4 = New TextBox()
+        Button1 = New Button()
+        DateTimePicker1 = New DateTimePicker()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI Semibold", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(263, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(258, 30)
+        Label1.TabIndex = 0
+        Label1.Text = "Library Registration Form"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(286, 48)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(217, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Fill out the form carefully or registration"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(23, 103)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(157, 20)
+        Label3.TabIndex = 2
+        Label3.Text = "Name of the appicant"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(23, 126)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(183, 28)
+        TextBox1.TabIndex = 3
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(304, 126)
+        TextBox2.Multiline = True
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(185, 28)
+        TextBox2.TabIndex = 4
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(583, 126)
+        TextBox3.Multiline = True
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(185, 28)
+        TextBox3.TabIndex = 5
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(23, 157)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(64, 15)
+        Label4.TabIndex = 6
+        Label4.Text = "First Name"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(304, 157)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(79, 15)
+        Label5.TabIndex = 7
+        Label5.Text = "Middle Name"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(583, 157)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(63, 15)
+        Label6.TabIndex = 8
+        Label6.Text = "Last Name"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label7.Location = New Point(23, 197)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(78, 20)
+        Label7.TabIndex = 9
+        Label7.Text = "Birth Date"
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(CheckBox2)
+        GroupBox1.Controls.Add(CheckBox1)
+        GroupBox1.Location = New Point(352, 220)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(200, 100)
+        GroupBox1.TabIndex = 16
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Gender"
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.Location = New Point(3, 44)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(64, 19)
+        CheckBox2.TabIndex = 1
+        CheckBox2.Text = "Female"
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(3, 19)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(52, 19)
+        CheckBox1.TabIndex = 0
+        CheckBox1.Text = "Male"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Font = New Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label11.Location = New Point(23, 341)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(63, 20)
+        Label11.TabIndex = 2
+        Label11.Text = "Address"
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(24, 364)
+        TextBox4.Multiline = True
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(465, 28)
+        TextBox4.TabIndex = 17
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.LimeGreen
+        Button1.Font = New Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(625, 309)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 32)
+        Button1.TabIndex = 18
+        Button1.Text = "Register"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' DateTimePicker1
+        ' 
+        DateTimePicker1.Location = New Point(24, 220)
+        DateTimePicker1.Name = "DateTimePicker1"
+        DateTimePicker1.Size = New Size(219, 23)
+        DateTimePicker1.TabIndex = 19
+        ' 
+        ' Form2
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(800, 450)
+        Controls.Add(DateTimePicker1)
+        Controls.Add(Button1)
+        Controls.Add(TextBox4)
+        Controls.Add(Label11)
+        Controls.Add(GroupBox1)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form2"
+        Text = "Form2"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label11 As Label
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DateTimePicker1 As DateTimePicker
 End Class
