b6e219f4122967d3eeb9286573121e810cc69f8d
iamjexcdevvv <dublasandrei@gmail.com>
2025-03-08 17:55:58 +0800

    added user registration backend

--- Form2.Designer.vb
+++ Form2.Designer.vb
@@ -22,24 +22,30 @@ Partial Class Form2
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form2))
         Label2 = New Label()
-        Label3 = New Label()
         TextBox1 = New TextBox()
-        TextBox2 = New TextBox()
         TextBox3 = New TextBox()
         Label4 = New Label()
-        Label5 = New Label()
         Label6 = New Label()
         Label7 = New Label()
         Button1 = New Button()
         TextBox5 = New TextBox()
         TextBox6 = New TextBox()
-        TextBox4 = New TextBox()
         Label1 = New Label()
         TextBox7 = New TextBox()
+        ErrorProvider1 = New ErrorProvider(components)
+        TextBox2 = New TextBox()
+        Label3 = New Label()
+        Label5 = New Label()
+        Button2 = New Button()
+        Label8 = New Label()
+        Label9 = New Label()
+        TextBox4 = New TextBox()
         RadioButton1 = New RadioButton()
         RadioButton2 = New RadioButton()
+        CType(ErrorProvider1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' Label2
@@ -53,39 +59,21 @@ Partial Class Form2
         Label2.TabIndex = 1
         Label2.Text = "FILL OUT THE FORM CAREFULLY FOR REGISTRATION !"
         ' 
-        ' Label3
-        ' 
-        Label3.AutoSize = True
-        Label3.BackColor = Color.DarkSalmon
-        Label3.Font = New Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        Label3.Location = New Point(12, 98)
-        Label3.Name = "Label3"
-        Label3.Size = New Size(186, 18)
-        Label3.TabIndex = 2
-        Label3.Text = "Name of the applicant:"
-        ' 
         ' TextBox1
         ' 
         TextBox1.BackColor = Color.DarkSalmon
-        TextBox1.Location = New Point(12, 121)
+        TextBox1.Font = New Font("Arial Rounded MT Bold", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox1.Location = New Point(69, 115)
         TextBox1.Multiline = True
         TextBox1.Name = "TextBox1"
         TextBox1.Size = New Size(227, 30)
         TextBox1.TabIndex = 3
         ' 
-        ' TextBox2
-        ' 
-        TextBox2.BackColor = Color.DarkSalmon
-        TextBox2.Location = New Point(254, 121)
-        TextBox2.Multiline = True
-        TextBox2.Name = "TextBox2"
-        TextBox2.Size = New Size(227, 30)
-        TextBox2.TabIndex = 4
-        ' 
         ' TextBox3
         ' 
         TextBox3.BackColor = Color.DarkSalmon
-        TextBox3.Location = New Point(496, 121)
+        TextBox3.Font = New Font("Arial Rounded MT Bold", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox3.Location = New Point(325, 115)
         TextBox3.Multiline = True
         TextBox3.Name = "TextBox3"
         TextBox3.Size = New Size(227, 30)
@@ -96,29 +84,18 @@ Partial Class Form2
         Label4.AutoSize = True
         Label4.BackColor = Color.DarkSeaGreen
         Label4.Font = New Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        Label4.Location = New Point(12, 154)
+        Label4.Location = New Point(69, 94)
         Label4.Name = "Label4"
         Label4.Size = New Size(94, 18)
         Label4.TabIndex = 6
         Label4.Text = "First Name"
-        ' 
-        ' Label5
-        ' 
-        Label5.AutoSize = True
-        Label5.BackColor = Color.DarkSeaGreen
-        Label5.Font = New Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        Label5.Location = New Point(254, 154)
-        Label5.Name = "Label5"
-        Label5.Size = New Size(113, 18)
-        Label5.TabIndex = 7
-        Label5.Text = "Middle  Name"
         ' 
         ' Label6
         ' 
         Label6.AutoSize = True
         Label6.BackColor = Color.DarkSeaGreen
         Label6.Font = New Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        Label6.Location = New Point(496, 154)
+        Label6.Location = New Point(325, 94)
         Label6.Name = "Label6"
         Label6.Size = New Size(93, 18)
         Label6.TabIndex = 8
@@ -129,7 +106,7 @@ Partial Class Form2
         Label7.AutoSize = True
         Label7.BackColor = Color.MediumAquamarine
         Label7.Font = New Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        Label7.Location = New Point(12, 198)
+        Label7.Location = New Point(325, 231)
         Label7.Name = "Label7"
         Label7.Size = New Size(118, 18)
         Label7.TabIndex = 9
@@ -138,9 +115,10 @@ Partial Class Form2
         ' Button1
         ' 
         Button1.BackColor = Color.DarkCyan
+        Button1.Cursor = Cursors.Hand
         Button1.Font = New Font("Arial Rounded MT Bold", 16.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Button1.ForeColor = SystemColors.ActiveCaptionText
-        Button1.Location = New Point(597, 352)
+        Button1.Location = New Point(325, 320)
         Button1.Name = "Button1"
         Button1.Size = New Size(163, 34)
         Button1.TabIndex = 14
@@ -152,7 +130,7 @@ Partial Class Form2
         TextBox5.BackColor = Color.OrangeRed
         TextBox5.Font = New Font("Arial Rounded MT Bold", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         TextBox5.Location = New Point(4, 8)
-        TextBox5.Margin = New Padding(2, 2, 2, 2)
+        TextBox5.Margin = New Padding(2)
         TextBox5.Name = "TextBox5"
         TextBox5.Size = New Size(789, 38)
         TextBox5.TabIndex = 0
@@ -162,29 +140,19 @@ Partial Class Form2
         ' TextBox6
         ' 
         TextBox6.BackColor = Color.MediumAquamarine
-        TextBox6.Location = New Point(12, 221)
-        TextBox6.Margin = New Padding(2, 2, 2, 2)
+        TextBox6.Font = New Font("Arial Rounded MT Bold", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox6.Location = New Point(325, 254)
+        TextBox6.Margin = New Padding(2)
         TextBox6.Name = "TextBox6"
-        TextBox6.Size = New Size(227, 23)
+        TextBox6.Size = New Size(227, 21)
         TextBox6.TabIndex = 15
-        ' 
-        ' TextBox4
-        ' 
-        TextBox4.BackColor = Color.MediumAquamarine
-        TextBox4.Font = New Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        TextBox4.Location = New Point(515, 193)
-        TextBox4.Margin = New Padding(2, 2, 2, 2)
-        TextBox4.Name = "TextBox4"
-        TextBox4.Size = New Size(74, 26)
-        TextBox4.TabIndex = 18
-        TextBox4.Text = "Gender:"
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
         Label1.BackColor = Color.MediumAquamarine
         Label1.Font = New Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        Label1.Location = New Point(274, 198)
+        Label1.Location = New Point(69, 231)
         Label1.Name = "Label1"
         Label1.Size = New Size(128, 18)
         Label1.TabIndex = 19
@@ -193,21 +161,108 @@ Partial Class Form2
         ' TextBox7
         ' 
         TextBox7.BackColor = Color.MediumAquamarine
-        TextBox7.Location = New Point(274, 221)
-        TextBox7.Margin = New Padding(2, 2, 2, 2)
+        TextBox7.Font = New Font("Arial Rounded MT Bold", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox7.Location = New Point(69, 254)
+        TextBox7.Margin = New Padding(2)
         TextBox7.Name = "TextBox7"
-        TextBox7.Size = New Size(227, 23)
+        TextBox7.Size = New Size(227, 21)
         TextBox7.TabIndex = 20
+        ' 
+        ' ErrorProvider1
+        ' 
+        ErrorProvider1.ContainerControl = Me
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.BackColor = Color.MediumAquamarine
+        TextBox2.Font = New Font("Arial Rounded MT Bold", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox2.Location = New Point(325, 187)
+        TextBox2.Margin = New Padding(2)
+        TextBox2.Name = "TextBox2"
+        TextBox2.PasswordChar = "*"c
+        TextBox2.Size = New Size(227, 21)
+        TextBox2.TabIndex = 23
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.BackColor = Color.MediumAquamarine
+        Label3.Font = New Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(325, 167)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(92, 18)
+        Label3.TabIndex = 24
+        Label3.Text = "Password:"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.BackColor = Color.MediumAquamarine
+        Label5.Font = New Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label5.Location = New Point(599, 94)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(73, 18)
+        Label5.TabIndex = 25
+        Label5.Text = "Gender:"
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.DarkCyan
+        Button2.Cursor = Cursors.Hand
+        Button2.Font = New Font("Arial Rounded MT Bold", 9.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button2.ForeColor = SystemColors.ActiveCaptionText
+        Button2.Location = New Point(666, 377)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(79, 32)
+        Button2.TabIndex = 26
+        Button2.Text = "Login"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.BackColor = Color.Transparent
+        Label8.Font = New Font("Arial Rounded MT Bold", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label8.Location = New Point(620, 360)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(157, 14)
+        Label8.TabIndex = 27
+        Label8.Text = "Already have an account?"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.BackColor = Color.MediumAquamarine
+        Label9.Font = New Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label9.Location = New Point(69, 167)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(95, 18)
+        Label9.TabIndex = 29
+        Label9.Text = "Username:"
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.BackColor = Color.MediumAquamarine
+        TextBox4.Font = New Font("Arial Rounded MT Bold", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox4.Location = New Point(69, 187)
+        TextBox4.Margin = New Padding(2)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(227, 21)
+        TextBox4.TabIndex = 28
         ' 
         ' RadioButton1
         ' 
         RadioButton1.AutoSize = True
         RadioButton1.BackColor = Color.MediumAquamarine
+        RadioButton1.Checked = True
+        RadioButton1.Cursor = Cursors.Hand
         RadioButton1.FlatStyle = FlatStyle.System
-        RadioButton1.Location = New Point(545, 223)
-        RadioButton1.Margin = New Padding(2, 2, 2, 2)
+        RadioButton1.Font = New Font("Arial Rounded MT Bold", 9.75F)
+        RadioButton1.ForeColor = Color.Black
+        RadioButton1.Location = New Point(599, 125)
+        RadioButton1.Margin = New Padding(2)
         RadioButton1.Name = "RadioButton1"
-        RadioButton1.Size = New Size(69, 20)
+        RadioButton1.Size = New Size(79, 20)
         RadioButton1.TabIndex = 21
         RadioButton1.TabStop = True
         RadioButton1.Text = "Female"
@@ -217,13 +272,15 @@ Partial Class Form2
         ' 
         RadioButton2.AutoSize = True
         RadioButton2.BackColor = Color.MediumAquamarine
+        RadioButton2.Cursor = Cursors.Hand
         RadioButton2.FlatStyle = FlatStyle.System
-        RadioButton2.Location = New Point(629, 223)
-        RadioButton2.Margin = New Padding(2, 2, 2, 2)
+        RadioButton2.Font = New Font("Arial Rounded MT Bold", 9.75F)
+        RadioButton2.ForeColor = Color.Black
+        RadioButton2.Location = New Point(683, 125)
+        RadioButton2.Margin = New Padding(2)
         RadioButton2.Name = "RadioButton2"
-        RadioButton2.Size = New Size(57, 20)
+        RadioButton2.Size = New Size(62, 20)
         RadioButton2.TabIndex = 22
-        RadioButton2.TabStop = True
         RadioButton2.Text = "Male"
         RadioButton2.UseVisualStyleBackColor = False
         ' 
@@ -236,41 +293,52 @@ Partial Class Form2
         ClientSize = New Size(800, 421)
         Controls.Add(RadioButton2)
         Controls.Add(RadioButton1)
+        Controls.Add(Label9)
+        Controls.Add(TextBox4)
+        Controls.Add(Label8)
+        Controls.Add(Button2)
+        Controls.Add(Label5)
+        Controls.Add(Label3)
+        Controls.Add(TextBox2)
         Controls.Add(TextBox7)
         Controls.Add(Label1)
-        Controls.Add(TextBox4)
         Controls.Add(TextBox6)
         Controls.Add(TextBox5)
         Controls.Add(Button1)
         Controls.Add(Label7)
         Controls.Add(Label6)
-        Controls.Add(Label5)
         Controls.Add(Label4)
         Controls.Add(TextBox3)
-        Controls.Add(TextBox2)
         Controls.Add(TextBox1)
-        Controls.Add(Label3)
         Controls.Add(Label2)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
         Name = "Form2"
-        Text = "Form2"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Registration"
+        CType(ErrorProvider1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
         PerformLayout()
     End Sub
     Friend WithEvents Label2 As Label
-    Friend WithEvents Label3 As Label
     Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
     Friend WithEvents TextBox3 As TextBox
     Friend WithEvents Label4 As Label
-    Friend WithEvents Label5 As Label
     Friend WithEvents Label6 As Label
     Friend WithEvents Label7 As Label
     Friend WithEvents Button1 As Button
     Friend WithEvents TextBox5 As TextBox
     Friend WithEvents TextBox6 As TextBox
-    Friend WithEvents TextBox4 As TextBox
     Friend WithEvents Label1 As Label
     Friend WithEvents TextBox7 As TextBox
-    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents ErrorProvider1 As ErrorProvider
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox4 As TextBox
     Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
 End Class
--- Form2.vb
+++ Form2.vb
@@ -1,3 +1,96 @@
-﻿Public Class Form2
+﻿Imports System.Text.RegularExpressions
+Imports MySql.Data.MySqlClient
 
+Public Class Form2
+    Private Function IsValidEmailFormat(input As String) As Boolean
+        Return Regex.IsMatch(input, "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
+    End Function
+    Private Function IsValidUserInput()
+        Dim isValidInput As Boolean = True
+
+        If TextBox1.Text = "" Then
+            ErrorProvider1.SetError(TextBox1, "Please fill this out!")
+            isValidInput = False
+        Else
+            ErrorProvider1.SetError(TextBox1, "")
+        End If
+
+        If TextBox3.Text = "" Then
+            ErrorProvider1.SetError(TextBox3, "Please fill this out!")
+            isValidInput = False
+        Else
+            ErrorProvider1.SetError(TextBox3, "")
+        End If
+
+        If TextBox6.Text = "" Then
+            ErrorProvider1.SetError(TextBox6, "Please fill this out!")
+            isValidInput = False
+        Else
+            ErrorProvider1.SetError(TextBox6, "")
+        End If
+
+        If TextBox2.Text = "" Then
+            ErrorProvider1.SetError(TextBox2, "Please fill this out!")
+            isValidInput = False
+        Else
+            ErrorProvider1.SetError(TextBox2, "")
+        End If
+
+        If TextBox7.Text = "" Then
+            ErrorProvider1.SetError(TextBox7, "Please fill this out!")
+            isValidInput = False
+        Else
+            If Not IsValidEmailFormat(TextBox7.Text) Then
+                ErrorProvider1.SetError(TextBox7, "Invalid email format")
+                isValidInput = False
+            Else
+                ErrorProvider1.SetError(TextBox7, "")
+            End If
+        End If
+
+        Return isValidInput
+    End Function
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If (IsValidUserInput()) Then
+            Dim password As String = BCrypt.Net.BCrypt.HashPassword(TextBox2.Text)
+            Dim studentName As String = TextBox1.Text + " " + TextBox3.Text
+            Dim studentGender As String = "Female"
+
+            If RadioButton2.Checked Then
+                studentGender = "Male"
+            End If
+
+            RegisterNewUser(TextBox4.Text, TextBox7.Text, password, studentName, studentGender)
+        End If
+    End Sub
+    Private Async Sub RegisterNewUser(userName As String, email As String, password As String, studentName As String, studentGender As String)
+        Try
+            Dim query As String = "INSERT INTO users (username, email, studentPassword, studentName, studentGender) " &
+                                  "VALUES (@username, @email, @studentPassword, @studentName, @studentGender)"
+
+            Using conn As New MySqlConnection(connectionString)
+                conn.Open()
+
+                Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@username", userName)
+                    cmd.Parameters.AddWithValue("@email", email)
+                    cmd.Parameters.AddWithValue("@studentPassword", password)
+                    cmd.Parameters.AddWithValue("@studentName", studentName)
+                    cmd.Parameters.AddWithValue("@studentGender", studentGender)
+
+                    Await cmd.ExecuteNonQueryAsync()
+
+                    Form1.Show()
+                    Me.Close()
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Form1.Show()
+        Me.Close()
+    End Sub
 End Class
